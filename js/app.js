0623acb58e73ed5459fe64b710bc1cc6525457e2
Nick Martinez <23nicholasmartinez@gmail.com>
2016-06-16 23:59:47 +0000

    Created function to alert if map error

--- js/app.js
+++ js/app.js
@@ -20,43 +20,50 @@ var surfSpots = [
   {
     name : 'Mavericks',
     latitude : '37.497882',
-    longitude : '-122.498801'
+    longitude : '-122.498801',
+    mark : ''
   },
 
   {
     name : 'Fort Point',
     latitude : '37.8088047',
-    longitude : '-122.4721241'
+    longitude : '-122.4721241',
+    mark : ''
   },
 
   {
     name : 'Stinson Beach',
     latitude : '37.9004',
-    longitude : '-122.6444'
+    longitude : '-122.6444',
+    mark : ''
   },
 
   {
     name : 'Steamer Lane',
     latitude : '36.9538399',
-    longitude : '-122.0241292'
+    longitude : '-122.0241292',
+    mark : ''
   },
 
   {
     name : 'Cowells Cove',
     latitude : '36.9624806',
-    longitude : '-122.0240314'
+    longitude : '-122.0240314',
+    mark : ''
   },
 
   {
     name : 'Bolinas',
     latitude : '37.9093',
-    longitude : '-122.6863'
+    longitude : '-122.6863',
+    mark : ''
   },
 
   {
     name : 'Montara',
     latitude : '37.5469',
-    longitude : '-122.5149'
+    longitude : '-122.5149',
+    mark : ''
   }
 ]
 
@@ -64,6 +71,7 @@ var Spot = function(data) {
   this.name = ko.observable(data.name);
   this.latitude = ko.observable(data.latitude);
   this.longitude = ko.observable(data.longitude);
+  this.mark = '';
 };
 
 var ViewModel = function() {
@@ -82,12 +90,16 @@ var ViewModel = function() {
   };
 };
 
-ko.applyBindings(new ViewModel());
-
 var map;
+var marker;
+var infoWindow;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.773972, lng: -122.431297},
     zoom: 8
   });
 }
+
+ko.applyBindings(new ViewModel());
+
